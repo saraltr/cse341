f754125f67c59196600df114caf8271462441f29
sara latorre <saralatorre97@gmail.com>
2023-09-13 20:24:14 +0200

    added routes and send request

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+
+app.use("/", require("./routes"))
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-}
-)
+    console.log("Web Server is listening at port " + (process.env.PORT || 3000));
+});
