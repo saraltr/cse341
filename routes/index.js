56f168183517474c96c0595739f9163e0c0e8a96
sara latorre <saralatorre97@gmail.com>
2023-09-13 20:56:36 +0200

    added json message

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require("express").Router();
 
 routes.get("/", (req, res, next) => {
-    res.json("Hello World, This is port 3000");
+    res.json("Hello World, This is port 3000");    
 });
 
 module.exports = routes;
